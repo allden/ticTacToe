a4281ff4ba65ce2d6800bddafe8a8c2b173b270a
Allden <laokahm@gmail.com>
2019-08-13 23:43:03 +0300

    I've added AI. The Minimax function was taken from https://github.com/ahmadabdolsaheb/minimaxarticle/blob/master/index.js

--- app.js
+++ app.js
@@ -2,125 +2,193 @@ let box = document.querySelectorAll('.box');
 let playerTrack = document.querySelector('.playerTurn');
 let startBtn = document.querySelector('.start');
 let robotBtn = document.querySelector('.robot-start');
-let boardFull = false;
 let gameState = false;    
 let initialStart = false;
 let robotMode = false;
 let playerTurn = 1;
-
+let board = [0,1,2,3,4,5,6,7,8];
+let playerOne = 'X';
+let playerTwo = 'O';
+let move = 0;
+// functions
 const game = (() => {
     // player / AI movement
     const playerMove = (e) => {
         if(gameState !== false) {
             if(playerTurn % 2 !== 0) {
                 if(e.target.classList.contains('full')) {
-                    game.showAlert('Select a different spot.');
-                } else {
-                    e.target.textContent = "X";
-                    playerTrack.textContent = `O's Turn`;
-                    e.target.classList.add('full');
-                    playerTurn++;
-                    
-                    game.victory();
+                    showAlert('Select a different spot.');
+                } else {        
                     if(robotMode === true && gameState !== false) {
                         setTimeout(robotMove, 1000);
                     }
+                    e.target.textContent = playerOne;
+                    boardTranscribe(e.target, playerOne);
+                    playerTrack.textContent = `${playerTwo}'s Turn`;
+                    e.target.classList.add('full');
+                    playerTurn++;
+                    
+                    victory();
                 }
 
             } else if(playerTurn % 2 === 0 && robotMode === false) {
                 if(e.target.classList.contains('full')) {
-                    game.showAlert('Select a different spot.');
-                } else {
-                    e.target.textContent = "O";
-                    playerTrack.textContent = `X's Turn`;
+                    showAlert('Select a different spot.');
+                } else {        
+                    e.target.textContent = playerTwo;
+                    playerTrack.textContent = `${playerOne}'s Turn`;
+                    boardTranscribe(e.target, playerTwo);
                     e.target.classList.add('full');
                     playerTurn++;
-                    game.victory();
+                    victory();
                 }
             }
             
         } else if(gameState == false && initialStart !== false) {
-            game.showAlert('Press "Restart" to play again.');
+            showAlert('Press "Restart" to play again.');
         } else {
-            game.showAlert('Press "Start" to begin.');
+            showAlert('Press "Start" to begin.');
         }
     }
-    
+    const boardTranscribe = (obj, player) => {
+        if(obj.classList.contains('space-1')) {
+            board[0] = player;
+        } else if(obj.classList.contains('space-2')) {
+            board[1] = player;
+        }
+        else if(obj.classList.contains('space-3')) {
+            board[2] = player;
+        }
+        else if(obj.classList.contains('space-4')) {
+            board[3] = player;
+        }
+        else if(obj.classList.contains('space-5')) {
+            board[4] = player;
+        }
+        else if(obj.classList.contains('space-6')) {
+            board[5] = player;
+        }
+        else if(obj.classList.contains('space-7')) {
+            board[6] = player;
+        }
+        else if(obj.classList.contains('space-8')) {
+            board[7] = player;
+        }
+        else {
+            board[8] = player;
+        } 
+    }
+
+    const minimax = (newBoard, player) => {
+        //available spots
+        var availSpots = emptyIndexies(newBoard);
+
+        // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+        if (checkBoard(newBoard, playerOne)){
+            return {score:-10};
+        } else if (checkBoard(newBoard, playerTwo)){
+            return {score:10};
+        } else if (availSpots.length === 0){
+            return {score:0};
+        }
+
+        // an array to collect all the objects
+        var moves = [];
+
+        // loop through available spots
+        for (var i = 0; i < availSpots.length; i++){
+        //create an object for each and store the index of that spot that was stored as a number in the object's index key
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+
+        // set the empty spot to the current player
+        newBoard[availSpots[i]] = player;
+
+        //if collect the score resulted from calling minimax on the opponent of the current player
+        if (player == playerTwo){
+        var result = minimax(newBoard, playerOne);
+        move.score = result.score;
+        }
+        else{
+        var result = minimax(newBoard, playerTwo);
+        move.score = result.score;
+        }
+
+        //reset the spot to empty
+        newBoard[availSpots[i]] = move.index;
+
+        // push the object to the array
+        moves.push(move);
+        }
+
+        var bestMove;
+        if(player === playerTwo){
+            var bestScore = -10000;
+            for(var i = 0; i < moves.length; i++){
+                if(moves[i].score > bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }else{
+
+        // else loop over the moves and choose the move with the lowest score
+            var bestScore = 10000;
+            for(var i = 0; i < moves.length; i++){
+            if(moves[i].score < bestScore){
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+            }
+        }
+
+        return moves[bestMove];
+    }
+
+    const emptyIndexies = (board) => {
+        return board.filter(empty => empty !== playerOne && empty !== playerTwo)
+    }
+
     const robotMove = () => {
-        let randMove = Math.floor(Math.random()*9);
-        if(box[randMove].classList.contains('full') === true && boardFull === false && gameState !== false) {
-            robotMove();
-            return;
-        } else if(boardFull === false && gameState !== false) {
-            box[randMove].textContent = "O";
-            playerTrack.textContent = `X's Turn`;
-            box[randMove].classList.add('full');
+        if(gameState !== false) {
+            let bestSpot = minimax(board, playerTwo);
+            console.log(bestSpot.index);
+            box[bestSpot.index].textContent = playerTwo;
+            boardTranscribe(box[bestSpot.index], playerTwo);
+            playerTrack.textContent = `${playerOne}'s Turn`;
+            box[bestSpot.index].classList.add('full');
             playerTurn++;
-            game.victory();
-        } else {
-            console.log('Stops working..');
+            victory();
         }
     }
     // based on the current iteration of i, insert it into a string for the row/column/diag then put it into list
     // if we end up with 'xxx' or 'ooo' inside of the list, the game is won
-    const checkBoard = () => {
-        let list = '';
-        let row = '';
-        let row1 = '';
-        let row2 = '';
-        let col = '';
-        let col1 = '';
-        let col2 = '';
-        let diag = '';
-        let counterDiag = '';
-        let fullBoxes = 0;
-        for(i = 0; i < box.length; i++) {
-            if(i === 0 || i === 1 || i === 2) {
-                row += box[i].textContent;
-            }
-            if(i === 3 || i === 4 || i === 5) {
-                row1 += box[i].textContent;
-            }
-            if(i === 6 || i === 7 || i === 8) {
-                row2 += box[i].textContent;
-            }
-            if(i === 0 || i === 3 || i === 6) {
-                col += box[i].textContent;
-            }
-            if(i === 1 || i === 4 || i === 7) {
-                col1 += box[i].textContent;
-            }
-            if(i === 2 || i === 5 || i === 8) {
-                col2 += box[i].textContent;
-            }
-            if(i === 0 || i === 4 || i === 8) {
-                diag += box[i].textContent;
-            }
-            if(i === 6 || i === 4 || i === 2) {
-                counterDiag += box[i].textContent;
-            }
-            if(box[i].textContent !== '') {
-                fullBoxes++;
-                if(fullBoxes === 9) {
-                    boardFull = true;
-                    fullBoxes = 0;
-                }
-            }
+    const checkBoard = (board, player) => {
+        if(
+            (board[0] === player && board[1] === player && board[2] === player) ||
+            (board[3] === player && board[4] === player && board[5] === player) ||
+            (board[6] === player && board[7] === player && board[8] === player) ||
+            (board[0] === player && board[3] === player && board[6] === player) ||
+            (board[1] === player && board[4] === player && board[7] === player) ||
+            (board[2] === player && board[5] === player && board[8] === player) ||
+            (board[0] === player && board[4] === player && board[8] === player) ||
+            (board[2] === player && board[4] === player && board[6] === player)
+        ) {
+            return true;
+        } else {
+            return false;
         }
-        list = `${row}, ${row1}, ${row2}, ${col}, ${col1}, ${col2}, ${diag}, ${counterDiag}`;
-        return list.toLowerCase();
     }
     // win condition
     const victory = () => {
-        let currentState = game.checkBoard();
-        if(currentState.indexOf('xxx') > -1) {
-            showAlert('X Wins!');
+        if(checkBoard(board, playerOne) === true) {
+            showAlert(`${playerOne} wins!`);
             gameState = false;
-        } else if (currentState.indexOf('ooo') > -1) {
-            showAlert('O Wins!');
+        } else if(checkBoard(board, playerTwo) === true) {
+            showAlert(`${playerTwo} wins!`);
             gameState = false;
-        } else if (currentState.indexOf('ooo') == -1 && currentState.indexOf('xxx') == -1 && boardFull == true) {
-            showAlert('It\'s a draw!');
+        } else if(emptyIndexies(board).length === 0) {
+            showAlert(`It's a tie!`);
             gameState = false;
         }
     }
@@ -130,6 +198,7 @@ const game = (() => {
             element.classList.remove('full');
             element.textContent = '';
         });
+        board = [0,1,2,3,4,5,6,7,8];
         playerTurn = 1;
         gameState = true;
         boardFull = false;
@@ -138,7 +207,6 @@ const game = (() => {
     const start = () => {
         restart();
         robotMode = false;
-        gameState = true;
         initialStart = true;
         startBtn.value = 'Restart';
         startBtn.removeEventListener('click', game.start);
@@ -150,7 +218,6 @@ const game = (() => {
 
     const robotStart = () => {
         restart();
-        gameState = true;
         initialStart = true;
         robotMode = true;
         startBtn.value = 'Restart';
@@ -186,7 +253,7 @@ const game = (() => {
         myAlert.remove();
     }
 
-    return { playerMove, checkBoard, victory, showAlert, hideAlert, start, robotStart, robotMove }
+    return { playerMove, start, robotStart }
 })();
 
 for(i = 0; i < box.length; i++) {
